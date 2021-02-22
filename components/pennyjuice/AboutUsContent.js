78db32f21b76eccbb8bdfa285ecac3aa9e31ae29
jamaihla <jamaihl.a@agentsofvalue.com>
2021-02-22 09:13:43 +0800

    Update content pages

--- components/pennyjuice/AboutUsContent.js
+++ components/pennyjuice/AboutUsContent.js
@@ -3,15 +3,15 @@ export default function AboutUsContent() {
     return (
         <div>
             <div className="w-screen sm:w-full px-10 py-24 lg:max-w-full xl:px-36 h-full mx-auto">
-                <h1 className="font-main text-5.5xl font-bold">About Us</h1>
+                <h1 className="font-main text-5.5xl font-bold py-5">About Us</h1>
 
-                <h3 className="font-main text-3.5xl font-bold text-lightblack">Premium Quality 100% Fruit Juice Concentrate. No Sugar Added. Non Refrigerated</h3>
+                <h3 className="font-main text-3.5xl font-bold text-lightblack py-5">Premium Quality 100% Fruit Juice Concentrate. No Sugar Added. Non Refrigerated</h3>
             
-                <p>
+                <p className="py-5 font-main">
                 Get delicious and nutritious 100% fruit juice from Penny Juice of America. We provide 100% blended fruit juice concentrates that are specially made for kids and meet all the requirements of the FDA with regard to subsidy and nutrition. Customers prefer our products for their benefits, features, variety, service, and competitive pricing.
                 </p>
 
-                <p>These blended fruit juice concentrates are for all childcare centers and preschools. Penny Juice of America uses only the highest quality fruit juice products.</p>
+                <p className="py-5 font-main">These blended fruit juice concentrates are for all childcare centers and preschools. Penny Juice of America uses only the highest quality fruit juice products.</p>
             </div>
         </div>
     )
